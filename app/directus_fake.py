a20ead4ddac1e76e2d81a16f20ba385ac88d83a7
Sabrina del Poy <sabriidelpoy@gmail.com>
2019-06-29 12:57:21 -0300

    manifiesto

--- app/directus_fake.py
+++ app/directus_fake.py
@@ -7,8 +7,8 @@ def get_index_textos():
             'boton': 'Ver más'},
         'manifiesto': {
             'titulo': 'Nuestro manifiesto',
-            'parrafo1': 'Est nobis dolorem magni qui rerum delectus laborum ipsum. Totam dolores totam aspernatur sed. Temporibus autem voluptatem sunt aliquam sit debitis et illo. Velit nostrum sit animi necessitatibus sed impedit delectus. Aperiam amet perferendis officiis ad amet ab voluptas. Suscipit ipsa reiciendis eveniet. Necessitatibus ipsum ut est repellendus exercitationem. Voluptate non aperiam accusamus omnis quia eaque eum nostrum. Modi quibusdam quam et. Corrupti officiis sunt sit et maxime sed ipsa modi. Ipsum eveniet omnis eum magni. Magni temporibus cupiditate eum culpa maxime. Harum esse rem ad nihil rerum. Provident aut qui mollitia neque earum minima ducimus. Nemo vel maxime unde et at. Quo cum doloremque ut ad in. Atque quia eveniet quas est non. Esse magnam qui qui inventore consequatur non itaque delectus. Veritatis laborum beatae est. Sed nostrum qui aperiam. Est laudantium dignissimos iste labore voluptatem ea. Corporis rerum quidem molestiae dolor.',
-            'parrafo2': 'Est nobis dolorem magni qui rerum delectus laborum ipsum. Totam dolores totam aspernatur sed. Temporibus autem voluptatem sunt aliquam sit debitis et illo. Velit nostrum sit animi necessitatibus sed impedit delectus. Aperiam amet perferendis officiis ad amet ab voluptas. Suscipit ipsa reiciendis eveniet. Necessitatibus ipsum ut est repellendus exercitationem. Voluptate non aperiam accusamus omnis quia eaque eum nostrum. Modi quibusdam quam et. Corrupti officiis sunt sit et maxime sed ipsa modi. Ipsum eveniet omnis eum magni. Magni temporibus cupiditate eum culpa maxime. Harum esse rem ad nihil rerum. Provident aut qui mollitia neque earum minima ducimus. Nemo vel maxime unde et at. Quo cum doloremque ut ad in. Atque quia eveniet quas est non. Esse magnam qui qui inventore consequatur non itaque delectus. Veritatis laborum beatae est. Sed nostrum qui aperiam. Est laudantium dignissimos iste labore voluptatem ea. Corporis rerum quidem molestiae dolor.'},
+            'parrafo1': 'Est nobis dolorem magni qui rerum delectus laborum ipsum. Totam dolores totam aspernatur sed. Temporibus autem voluptatem sunt aliquam sit debitis et illo. Velit nostrum sit animi necessitatibus sed impedit delectus. Aperiam amet perferendis officiis ad amet ab voluptas. Suscipit ipsa reiciendis eveniet. Necessitatibus ipsum ut est repellendus exercitationem. Voluptate non aperiam accusamus omnis quia eaque eum nostrum. Modi quibusdam quam et. Corrupti officiis sunt sit et maxime sed ipsa modi. Ipsum eveniet omnis eum magni.',
+            'firma': 'Causas Comunes'},
         'propuestas': {
             'titulo': 'Conocé las propuestas',
             'subtitulo': 'Est nobis dolorem magni qui rerum delectus laborum ipsum. Totam dolores totam aspernatur sed. Temporibus autem voluptatem sunt aliquam sit debitis et illo. Velit nostrum sit animi necessitatibus sed impedit delectus.'},
@@ -31,6 +31,9 @@ def get_index_imgs():
     return {
         'portada': {
             'imagen': 'portada.svg',
+        },
+        'manifiesto': {
+            'imagen': 'manifiesto.svg',
         },
         'candidates': {
             'imagen': 'nove2.jpg'
