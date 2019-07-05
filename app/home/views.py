57d7cb998049f8f24b60b730bdeb39cea2b106f2
Sabrina del Poy <sabriidelpoy@gmail.com>
2019-07-05 13:25:31 -0300

    Estructura base para seccion

--- app/home/views.py
+++ app/home/views.py
@@ -33,3 +33,35 @@ def index():
         itemsagenda=itemsagenda,
         itemspropuestas=itemspropuestas,
         isstatic=isstatic)
+
+@blueprint.route("/genero", methods=['GET'])
+def genero():
+    isstatic = not current_app.config['USE_DIRECTUS']
+    if isstatic:
+        import app.directus_fake as directus_fake
+        dtextos = directus_fake.get_genero_textos()
+        dimgs = directus_fake.get_index_imgs()
+        dimgsgenero = directus_fake.get_genero_imgs()
+        itemsseguidores = directus_fake.get_seguidor_items()
+        itemsnovedades = directus_fake.get_novedades_items()
+        itemsagenda = directus_fake.get_agenda_items()
+        itemstemas = directus_fake.get_temas_items()
+    else:
+        import app.directus as directus
+        dtextos = directus.dapi.get_textos('Genero')
+        dimgs = directus.dapi.get_imgs('Home')
+        dimgsgenero = directus.dapi.get_imgs('Genero')
+        itemsseguidores = directus.dapi.get_itemsseguidores()
+        itemsnovedades = directus.dapi.get_itemsnovedades()
+        itemsagenda = directus.dapi.get_itemsagenda()
+        itemstemas = directus.dapi.get_itemstemas()
+    return render_template(
+        'genero.html',
+        dtextos=dtextos,
+        dimgs=dimgs,
+        dimgsgenero=dimgsgenero,
+        itemsseguidores=itemsseguidores,
+        itemsnovedades=itemsnovedades,
+        itemsagenda=itemsagenda,
+        itemstemas=itemstemas,
+        isstatic=isstatic)
