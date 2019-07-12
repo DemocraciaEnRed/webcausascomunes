4f9f48f5e4ddc510b5caf47fd1d6d48bf0cb0a28
Wencha <wencha_@hotmail.com>
2019-07-12 16:38:07 -0300

    video en causas, pagina contacto, wipeamos directus_fake

--- app/home/views.py
+++ app/home/views.py
@@ -17,24 +17,17 @@ def after_request(response):
 
 @blueprint.route("/", methods=['GET'])
 def index():
-    isstatic = not current_app.config['USE_DIRECTUS']
-    if isstatic:
-        import app.directus_fake as directus_fake
-        dtextos = directus_fake.get_index_textos()
-        dimgs = directus_fake.get_index_imgs()
-        itemsnovedades = directus_fake.get_novedades_items()
-        itemsagenda = directus_fake.get_agenda_items()
-        itemspropuestas = directus_fake.get_propuesta_items()
-        dimgsfooter = directus_fake.get_footer_imgs()
-    else:
-        import app.directus as directus
-        dimgsnav = directus.dapi.get_imgs_pagina('Navegacion')
-        dimgsfooter = directus.dapi.get_imgs_pagina('Footer')
-        dtextos = directus.dapi.get_textos_pagina('Home')
-        dimgs = directus.dapi.get_imgs_pagina('Home')
-        itemspropuestas = directus.dapi.get_items_propuestas()
-        itemsnovedades = directus.dapi.get_items_novedades('Home')
-        itemsagenda = directus.dapi.get_items_agenda('Home')
+    isstatic = False
+
+    import app.directus as directus
+    dimgsnav = directus.dapi.get_imgs_pagina('Navegacion')
+    dimgsfooter = directus.dapi.get_imgs_pagina('Footer')
+    dtextos = directus.dapi.get_textos_pagina('Home')
+    dimgs = directus.dapi.get_imgs_pagina('Home')
+    itemspropuestas = directus.dapi.get_items_propuestas()
+    itemsnovedades = directus.dapi.get_items_novedades('Home')
+    itemsagenda = directus.dapi.get_items_agenda('Home')
+
     return render_template(
         'index.html',
         dimgsnav=dimgsnav,
@@ -44,6 +37,26 @@ def index():
         itemsnovedades=itemsnovedades,
         itemsagenda=itemsagenda,
         itemspropuestas=itemspropuestas,
+        isstatic=isstatic)
+
+
+@blueprint.route("/contacto", methods=['GET'])
+def contacto():
+    isstatic = False
+
+    import app.directus as directus
+    dimgsnav = directus.dapi.get_imgs_pagina('Navegacion')
+    dimgsfooter = directus.dapi.get_imgs_pagina('Footer')
+
+    dtextos = directus.dapi.get_textos_pagina('Contacto')
+    dimgs = directus.dapi.get_imgs_pagina('Contacto')
+
+    return render_template(
+        'contacto.html',
+        dimgsnav=dimgsnav,
+        dimgsfooter=dimgsfooter,
+        dtextos=dtextos,
+        dimgs=dimgs,
         isstatic=isstatic)
 
 
