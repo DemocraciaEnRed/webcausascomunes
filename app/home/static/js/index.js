77069ec951ff038d8f269feea51b7b814885bbe7
Sabrina del Poy <sabriidelpoy@gmail.com>
2019-07-12 18:36:51 -0300

    Ajustes menu

--- app/home/static/js/index.js
+++ app/home/static/js/index.js
@@ -1,6 +1,19 @@
-$('.scrollTo').click(function() {
+// to top right away
+if ( window.location.hash ) scroll(0,0);
+// void some browsers issue
+setTimeout( function() { scroll(0,0); }, 1);
+
+$(document).ready(function(){
+  $('.scrollTo').click(function(e) {
+    e.preventDefault();
     var sectionTo = $(this).attr('href');
     $('html, body').animate({
       scrollTop: $(sectionTo).offset().top
     }, 1000);
 });
+  if(window.location.hash) {
+    $('html, body').animate({
+      scrollTop: $(window.location.hash).offset().top
+    }, 1000);
+  }
+})
