aa1d0c96dcfb7dad63726b76acd29d68061133e4
Sabrina del Poy <sabriidelpoy@gmail.com>
2019-07-23 20:40:25 -0300

    carousel imagenes hackaton con blueimp

--- app/home/static/js/index.js
+++ app/home/static/js/index.js
@@ -22,30 +22,6 @@ $(document).ready(function(){
 
 //https://github.com/blueimp/Gallery
 function initBlueimp(){
-    document.getElementById('links').onclick = function(event) {
-        event = event || window.event
-        var target = event.target || event.srcElement,
-            link = target.src ? target.parentNode : target,
-            options = {
-                index: link, event: event,
-                onslide: function (index, slide) {
-                    self = this;
-                    var initializeAdditional = function (index, data, klass, self) {
-                        var text = self.list[index].getAttribute(data),
-                        node = self.container.find(klass);
-                        node.empty();
-                        if (text) {
-                            node[0].appendChild(document.createTextNode(text));
-                        }
-                    };
-                    initializeAdditional(index, 'data-description', '.description', self);
-                }
-            },
-            links = this.getElementsByTagName('a')
-        blueimp.Gallery(links, options)
-    }
-
-
     blueimp.Gallery(document.getElementById('links').getElementsByTagName('a'), {
         container: '#blueimp-gallery-carousel',
         carousel: true
