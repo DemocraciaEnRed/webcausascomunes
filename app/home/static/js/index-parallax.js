8bccc384647481a905b67c3c5675b897eedb328e
Wencha <wencha_@hotmail.com>
2019-08-21 16:07:41 -0300

    agregado parámetro speed y wrapper html

--- app/home/static/js/index-parallax.js
+++ app/home/static/js/index-parallax.js
@@ -11,50 +11,55 @@ $(function(){
 		* @param {string} imgFileName - Nombre de la imagen de la mancha, sin path, con extension
 		* @param {float} topPerc - Offset top porcentual (0.0-1.0) desde el top de la section attacheada
 		*/
-		constructor(cssId, imgFileName, topPerc) {			
+		constructor(cssId, imgFileName, topPerc, speed) {			
 			this.cssId = 'mancha-' + cssId;
 			this.imgFileName = imgFileName;
 			this.topPerc = topPerc;
+			this.speed = speed || 1;
 			//this.section = null;
 		}
 		
-		createHtml(attachToSectionJq){
+		createHtml(attachToEleJq){
 			//create ele from template
 			this.eleJq = Mancha.templateEleJq.clone()
 			
-			//set id and img
+			//set id and img and vel
 			this.eleJq.attr("id",  this.cssId)
 			this.eleJq.find('img').attr("src", Mancha.imgPath + '/' + this.imgFileName);
+			this.eleJq.attr("data-rellax-speed",  this.speed)
 			
 			//set top
-			var sectionTop = attachToSectionJq.offset().top
-			var sectionHei = attachToSectionJq.outerHeight()
-			this.eleJq.css({ top: sectionTop+sectionHei*this.topPerc });	
+			var sectionTop = attachToEleJq.offset().top
+			var sectionHei = attachToEleJq.outerHeight()
+			this.eleJq.find('img').css({ top: sectionHei*this.topPerc });	
 			
 			//add ele to html
-			attachToSectionJq.append(this.eleJq)
+			attachToEleJq.append(this.eleJq)
 		}
 	}
 	Mancha.imgPath = _data_parallax_img_path
 	Mancha.templateEleJq = $('.manchas-template')
+	Mancha.wrapperCssClass = 'manchas-wrapper'
 	
 	var sectionManchas = [
 		// mirar class Mancha constructor() de arriba para entender valores
 		{sectionEleJq: $('#manifiesto'), manchas: [
 			new Mancha('mani1', 'group-11.svg', 0.18),
-			new Mancha('mani2', 'group-12.svg', 0.22),
+			new Mancha('mani2', 'group-12.svg', 0.22,2),
 			new Mancha('mani3', 'group-13.svg', 0.28),
-			new Mancha('mani4', 'group-14.svg', 0.57)]},
+			new Mancha('mani4', 'group-14.svg', 0.57, 3)]},
 		{sectionEleJq: $('#wikis'), manchas: [
-			new Mancha('wiki1', 'group-32.svg', 0.60),
-			new Mancha('wiki2', 'group-31.svg', 0.22)]}
+			new Mancha('wiki2', 'group-31.svg', 0.32),
+			new Mancha('wiki1', 'group-32.svg', 0.60, 2)]}
 	]
 	
 	for (i in sectionManchas){
 		section = sectionManchas[i]
+		manchasWrapperJq = $('<div/>', {class: Mancha.wrapperCssClass})
+		section.sectionEleJq.append(manchasWrapperJq)
 		for (j in section.manchas){
 			mancha = section.manchas[j]
-			mancha.createHtml(section.sectionEleJq)
+			mancha.createHtml(manchasWrapperJq)
 		}
 	}
 	this.rellax = new Rellax('.manchas', {
