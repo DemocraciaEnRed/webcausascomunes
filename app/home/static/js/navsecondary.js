871a9598814667e349b4438a4800c725c606a58e
Wencha <wencha_@hotmail.com>
2019-09-04 16:51:40 -0300

    algunos fixes del nav secundario y comentado para el release, temporalmente

--- app/home/static/js/navsecondary.js
+++ app/home/static/js/navsecondary.js
@@ -2,6 +2,16 @@ $('.scrollTo').click(function() {
     var sectionActive = $('#nav-secondary .nav-link.active').attr('href')
     var sectionTo = $(this).attr('href');
     var scrollTo = $(sectionTo).offset().top - $('.nav-secondary').outerHeight() - $('#navbar').outerHeight();
+	
+	// estos son solo patches, el bug real está en que el .nav-secondary. No se tiene que hacer fixed,
+	// o al menos al hacerse, no debe correr todo el dom para arriba (mirar el nav principal que lo hace bien)
+	if (sectionTo == '#definicion')
+		scrollTo += $('#navbar').outerHeight();
+	// si estás en el header, cuando scrolles para abajo el .nav-secondary se va a hacer fixed, por ende saliendo
+	// del dom y restándole su height al resto de la página; efecto: el scrollto cae en cualquier lugar
+	// fix:
+	if (sectionTo != '#definicion' && ! $('.nav-secondary').hasClass('position-fixed'))
+		scrollTo -= $('.nav-secondary').outerHeight() - 4/*sino cae unos pixels en la sección anterior*/;
 
     $('html, body').animate({
       scrollTop: scrollTo
